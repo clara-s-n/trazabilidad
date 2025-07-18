6f31428ee1216741a8ed3c4e991505f4f3bc5ac2
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2025-07-18 12:34:11 -0300

    Levanta.

--- backend/services/database.service.ts
+++ backend/services/database.service.ts
@@ -10,8 +10,8 @@ const pgConfig: PoolConfig = {
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432", 10),
   user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "mydatabase",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "DWDatabase",
 };
 
 export const myPool = new Pool(pgConfig);
