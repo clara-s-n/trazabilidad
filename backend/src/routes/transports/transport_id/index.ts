96b0c20b0761f10647a4d5ad7b5e2397f2b675cb
Ana <ana.sena@correo.ucu.edu.uy>
2025-07-22 12:39:40 -0300

    Cambios en el readme

--- backend/src/routes/transports/transport_id/index.ts
+++ backend/src/routes/transports/transport_id/index.ts
@@ -20,6 +20,44 @@ const transportesRoutes: FastifyPluginAsync = async (fastify) => {
       throw new Error("Not implemented");
     },
   });
+
+  fastify.put('/:transport_id', {
+    schema: {
+      tags: ['Transportes'],
+      params: TransportParams,
+      description: 'Modificar un transporte',
+      summary: 'Realizar la modificación de un transporte',
+      security: [
+        {
+          bearerAuth: []
+        }
+      ],
+    },
+    onRequest: fastify.verifyOperatorOrAdmin,
+    handler: async (request, reply) => {
+      // Handle updating transporte by ID logic
+      throw new Error("Not implemented");
+    },
+  });
+
+  fastify.delete('/:transport_id', {
+    schema: {
+      tags: ['Transportes'],
+      params: TransportParams,
+      description: 'Eliminar un transporte',
+      summary: 'Realizar la eliminación de un transporte',
+      security: [
+        {
+          bearerAuth: []
+        }
+      ],
+    },
+    onRequest: fastify.verifyOperatorOrAdmin,
+    handler: async (request, reply) => {
+      // Handle deleting transporte by ID logic
+      throw new Error("Not implemented");
+    },
+  });
 };
 
 export default transportesRoutes;
