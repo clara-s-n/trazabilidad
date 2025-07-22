ed0ab5fd055f05151e21c91f55e759856b8bc9b2
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2025-07-22 19:01:49 -0300

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,22 +7,4 @@ export const routes: Routes = [
     loadComponent:() => 
       import('./routes/auth/login/login.page').then((m) => m.LoginPage),
   },
-    {
-    path: 'home',
-    pathMatch: 'full',
-    loadComponent:() => 
-      import('./routes/home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    loadComponent:() => 
-      import('./routes/home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'protegida',
-    pathMatch: 'full',
-    loadComponent:() => 
-      import('./routes/protegida/protegida.page').then((m) => m.ProtegidaHome),
-  },
 ];
