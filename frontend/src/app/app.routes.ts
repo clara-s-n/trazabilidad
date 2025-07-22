202d8df298023b331d1d47fd3411ca306aa40065
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2025-07-21 21:12:35 -0300

    Clase del lunes 21/7

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth/login',
+    pathMatch: 'full',
+    loadComponent:() => 
+      import('./routes/auth/pages/login/login.page').then((m) => m.LoginPage),
+  },
+  {
     path: '',
     redirectTo: 'folder/inbox',
     pathMatch: 'full',
