40ee3f9075bc4c831c5dc94451b01d760ab005e1
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2025-07-23 12:14:54 -0300

    Creación de páginas faltantes.

---
+++ frontend/src/app/routes/land/list/list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+  standalone: true,
+})
+export class ListPage  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
