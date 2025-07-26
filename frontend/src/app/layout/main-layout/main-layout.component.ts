b0fb1922a359f239cf318e0959ed00d1acd56cd8
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2025-07-26 16:38:11 -0300

    Reset el filtro fallido de menú.

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Router, Route, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MainStoreService } from 'src/app/services/main-store.service';
 import { MenuSection } from 'src/app/model/components/menuSection';
 import { CommonModule } from '@angular/common';
 import { IonAccordion, IonAccordionGroup, IonButtons, IonContent, IonHeader, IonItem, IonItemDivider, IonLabel, IonList, IonMenu, IonMenuButton, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { MenuRouteData } from 'src/app/model/components/menuRouteData';
 
 @Component({
   selector: 'app-main-layout',
@@ -31,39 +30,33 @@ import { MenuRouteData } from 'src/app/model/components/menuRouteData';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
-  public sections: MenuSection[] = [];
-  private router = inject(Router)
-  private mainStoreService = inject(MainStoreService)
+    private mainStoreService = new MainStoreService();
 
-  ngOnInit() {
-    // 1) Tomamos las rutas hijas del root ('')
-    const rootConfig = this.router.config.find(r => r.path === '');
-    const childRoutes = rootConfig?.children ?? [];
+  public sections: MenuSection[] = [
+    { label: 'Animales', color: 'tertiary', items: [
+        { label: 'Lista de animales', link: '/animal/list' },
+        { label: 'Crear un animal',   link: '/animal/create' }
+      ]
+    },
+    { label: 'Predios', color: 'light', items: [
+        { label: 'Lista de predios',  link: '/land/list' }
+      ]
+    },
+    { label: 'Usuarios', color: 'light', items: [
+        { label: 'Lista de usuarios', link: '/user/list' },
+        { label: 'Crear un usuario',  link: '/auth/register' }
+      ]
+    },
+    { label: 'Eventos', color: 'light', items: [
+        { label: 'Lista de eventos',  link: '/evento/pesaje/list' }
+      ]
+    }
+  ];
 
-    // 2) Filtramos solo las que tienen data.menu = true y no llevan ':'
-    const menuRoutes = childRoutes.filter((r): r is Route & { data: MenuRouteData } => {
-      const data = r.data as MenuRouteData;
-      return data?.['menu'] === true && !r.path?.includes(':');
-    });
-    console.log('Rutas con menú:', menuRoutes);
+  constructor() {}
 
-    // 3) Agrupamos por sección
-    const grouped = menuRoutes.reduce((acc, r) => {
-      const data = r.data as MenuRouteData;
-      const section = data['section']!;
-      const title   = data['title']!;
-      const link    = `/${r.path}`;
+  ngOnInit() {}
 
-      if (!acc[section]) {
-        acc[section] = { label: section , items: [] };
-      }
-      acc[section].items.push({ label: title, link });
-      return acc;
-    }, {} as Record<string, MenuSection>);
-
-    // 4) Convertimos el objeto agrupado en un array
-    this.sections = Object.values(grouped);
-  }
 
   logout() {
     this.mainStoreService.clearAuth();
