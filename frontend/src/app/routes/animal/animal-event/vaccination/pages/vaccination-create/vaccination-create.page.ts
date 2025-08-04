d057f5cad19f6b6b30b8cbf00f2b6b46256b71ff
Francisco Lima <francisco.lima@correo.ucu.edu.uy>
2025-08-03 22:24:05 -0300

    Implementación de páginas de vacunación.

--- frontend/src/app/routes/animal/animal-event/vaccination/pages/vaccination-create/vaccination-create.page.ts
+++ frontend/src/app/routes/animal/animal-event/vaccination/pages/vaccination-create/vaccination-create.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  input,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VaccinationService } from 'src/app/services/events/vaccination/vaccination.service';
+import { VaccinationCreate } from 'src/app/model/events/vaccination';
+import { IonicModule } from '@ionic/angular';
+import { VaccinationFormComponent } from '../../components/vaccination-form/vaccination-form.component';
 @Component({
   selector: 'app-vaccination-create',
   templateUrl: './vaccination-create.page.html',
   styleUrls: ['./vaccination-create.page.scss'],
   standalone: true,
+  imports: [IonicModule, VaccinationFormComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'page-vaccination-create' },
 })
-export class VaccinationCreatePage  implements OnInit {
+export class VaccinationCreatePage {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private vaccinationService = inject(VaccinationService);
 
-  constructor() { }
+  public id = input.required<string>();
 
-  ngOnInit() {}
-
+  async handleSave(data: VaccinationCreate) {
+    try {
+      await this.vaccinationService.createVaccination(this.id(), data);
+      this.router.navigate(['/animal/events/vaccination', this.id()]);
+    } catch (err) {
+      console.error('Error creando vacunación', err);
+    }
+  }
 }
--- frontend/src/app/routes/animal/animal-event/vaccination/pages/vaccination-list/vaccination-list.page.ts
+++ frontend/src/app/routes/animal/animal-event/vaccination/pages/vaccination-list/vaccination-list.page.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  inject,
+  resource,
+} from '@angular/core';
+import { VaccinationService } from 'src/app/services/events/vaccination/vaccination.service';
+import { Router } from '@angular/router';
 
+import {
+  IonHeader,
+  IonButtons,
+  IonToolbar,
+  IonTitle,
+  IonButton,
+  IonContent,
+  IonList,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonSpinner,
+  IonText,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { eyeOutline } from 'ionicons/icons';
+import { Vaccination } from 'src/app/model/events/vaccination';
 @Component({
   selector: 'app-vaccination-list',
   templateUrl: './vaccination-list.page.html',
   styleUrls: ['./vaccination-list.page.scss'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButton,
+    IonContent,
+    IonList,
+    IonCard,
+    IonButtons,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonSpinner,
+    IonText,
+  ],
 })
-export class VaccinationListPage  implements OnInit {
+export class VaccinationListPage {
+  constructor() {
+    addIcons({
+      eyeOutline,
+    });
+  }
 
-  constructor() { }
+  readonly id = input.required<string>();
+  private readonly vaccinationService = inject(VaccinationService);
+  private readonly router = inject(Router);
 
-  ngOnInit() {}
+  readonly vaccinationsResource = resource<Vaccination[], undefined>({
+    loader: async () =>
+      await this.vaccinationService.getAllVaccinations(this.id()),
+  });
 
+  goToCreate() {
+    this.router.navigate([`/animal/events/${this.id()}/vaccination/create`]);
+  }
+
+  reload() {
+    window.location.reload();
+  }
 }
