a6e033925b1b0da5eaea94917ba21ebcf6a17a0e
Ana Clara Sena <ana.sena@correo.ucu.edu.uy>
2025-08-05 17:46:55 -0300

    Registro de usuarios

--- frontend/src/app/routes/auth/register/register.page.ts
+++ frontend/src/app/routes/auth/register/register.page.ts
@@ -1,14 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import {
-  IonButton,
-  IonCol,
-  IonRow,
-  IonGrid,
-  IonInput,
-  IonItem,
   IonHeader,
   IonToolbar,
   IonTitle,
@@ -16,7 +8,8 @@ import {
   IonButtons,
   IonBackButton,
 } from '@ionic/angular/standalone';
-import { AuthService } from 'src/app/services/auth.service';
+import UserFormComponent from '../../../routes/user/components/user-form/user-form.component';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -24,61 +17,37 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.page.scss'],
   standalone: true,
   imports: [
-    CommonModule,
-    FormsModule,
-    IonButton,
-    IonCol,
-    IonRow,
-    IonGrid,
-    IonInput,
-    IonItem,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
     IonButtons,
     IonBackButton,
+    UserFormComponent
   ],
 })
 export class RegisterPage {
-  email = signal<string>('');
-  password = signal<string>('');
-  confirmPassword = signal<string>('');
-  loading = signal<boolean>(false);
-  error = signal<string>('');
+  loading = signal(false);
 
+ private userService = inject(UserService);
   private router = inject(Router);
-  private authService = inject(AuthService);
 
-  async onSubmit() {
-    if (!this.email() || !this.password() || !this.confirmPassword()) {
-      this.error.set('Todos los campos son requeridos');
-      return;
+  async onSubmit(data: { email: string; password: string; repeatPassword?: string; role_id: number }) {
+    const user: any = {
+      email: data.email,
+      password: data.password,
+      role_id: data.role_id
+    };
+    if (data.repeatPassword) {
+      user.repeatPassword = data.repeatPassword;
     }
-
-    if (this.password() !== this.confirmPassword()) {
-      this.error.set('Las contraseñas no coinciden');
-      return;
-    }
-
-    this.loading.set(true);
-    this.error.set('');
-
     try {
-      await this.authService.register(this.email(), this.password());
-
-      // Redirect to login after successful registration
+      const usuarioCreado = await this.userService.postUser(user);
       this.router.navigate(['/auth/login']);
     } catch (error) {
-      console.error('Register failed:', error);
-      this.error.set(
-        'Error al registrar usuario. Por favor, inténtalo de nuevo.'
-      );
-    } finally {
-      this.loading.set(false);
+      console.error('Error al crear el usuario:', error);
     }
   }
-
   goToLogin() {
     this.router.navigate(['/auth/login']);
   }
